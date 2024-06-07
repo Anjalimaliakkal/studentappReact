d768fe8f658fe185359b5299cceacab857b030ce
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-07 12:28:46 +0530

    project completed

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <NavBar/>
+            <h1><center>VIEWALL STUDENTS</center></h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div className="card">
+                                    <img src="https://www.stewardtoday.com/wp-content/uploads/2017/07/Female-College-Student-FB-1024x683.jpg" className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Student</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
